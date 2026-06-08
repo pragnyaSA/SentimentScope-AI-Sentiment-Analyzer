e73a607cbe371a42a99e8df09eb2420b3bbefc33
pragnyaSA <131436691+pragnyaSA@users.noreply.github.com>
2026-06-08 14:22:01 +0530

    update firebase config

--- frontend/src/firebase-config.js
+++ frontend/src/firebase-config.js
@@ -1,4 +1,3 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import {
   getAuth,
@@ -7,22 +6,17 @@ import {
   createUserWithEmailAndPassword,
   signOut,
 } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBP-Z0NNQEQ3zlZKtA0h0ffORSR11cnhMg",
-  authDomain: "anuwad-d169a.firebaseapp.com",
-  projectId: "anuwad-d169a",
-  storageBucket: "anuwad-d169a.firebasestorage.app",
-  messagingSenderId: "491992642923",
-  appId: "1:491992642923:web:75904b4310a19c7873082e",
-  measurementId: "G-69JECWRC4X"
+  apiKey: "AIzaSyC9KUGFYrmbDl1qPTsOjsBXkytJ7Pfhqfs",
+  authDomain: "sentimentscope-a6714.firebaseapp.com",
+  projectId: "sentimentscope-a6714",
+  storageBucket: "sentimentscope-a6714.firebasestorage.app",
+  messagingSenderId: "188551643830",
+  appId: "1:188551643830:web:ce7e0ea793d63923839c17",
+  measurementId: "G-J8595DBXKY"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
